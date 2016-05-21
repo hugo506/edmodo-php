649b5bf7e5207b62b8fef1a316497fb912e3c7e1
Hugo Alvarado <hugo102@gmail.com>
2016-05-21 12:23:01 -0600

    Added inital tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\Domain\Student;
+
 class ExampleTest extends TestCase
 {
     /**
@@ -11,9 +13,41 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicExample()
+    public function skip_testBasicExample()
     {
         $this->visit('/')
              ->see('Laravel 5');
+    }
+    
+    public function testLoginPageHasUsernameText()
+    {
+    	$this->visit('/')
+    		->see('Username');
+    }
+    
+    public function testStudentEntityHasName()
+    {
+    	$name = 'testname';
+    	
+    	$student = new Student(0, $name, '');
+    	    	
+    	$this->assertEquals($name, $student->name);    	    
+    }
+    
+    
+    public function testLoginFail()
+    {
+    	$response = $this->call('POST', 'login');
+    	
+    	$this->assertEquals(200, $response->status());
+    	$this->assertContains('Login Error', $response->getContent());
+    }
+    
+    public function testLoginPasses()
+    {
+    	$response = $this->call('POST', 'login', ['username' => 'student']);
+    	    	
+    	$this->assertEquals(302, $response->status());
+    	$this->assertContains('dashboard', $response->headers->get('Location'));
     }
 }
